a9c6be79215f5791488c6e1757c030981caf971e
aruncumer <arun.kumarhr007@gmail.com>
2021-06-10 22:47:48 +0530

    select items on click edit

--- src/app/feature-modules/contact/contact.component.ts
+++ src/app/feature-modules/contact/contact.component.ts
@@ -1,13 +1,33 @@
-import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
 
 @Component({
 	selector: 'app-contact',
 	templateUrl: './contact.component.html',
 	styleUrls: ['./contact.component.scss']
 })
+
 export class ContactComponent implements OnInit {
+	toggleFlag: Boolean;
+	visible = true;
+	selectable = true;
+	removable = true;
+	separatorKeysCodes: number[] = [ENTER, COMMA];
+	fruitCtrl = new FormControl();
+	filteredFruits: Observable<string[]>;
+	fruits: string[] = [];
+	allFruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+
+	@ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
+	@ViewChild('auto') matAutocomplete: MatAutocomplete;
 
 	constructor(
 		private matIconRegistry: MatIconRegistry,
@@ -18,9 +38,49 @@ export class ContactComponent implements OnInit {
 			"unicorn",
 			this.domSanitizer.bypassSecurityTrustResourceUrl("assets/Logos/thumbsup.svg")
 		);
+
+		this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
+			startWith(null),
+			map((fruit: string | null) => fruit ? this._filter(fruit) : this.allFruits.slice()));
 	}
 
 	ngOnInit(): void {
+		this.toggleFlag = true;
 	}
+
+	add(event: MatChipInputEvent): void {
+		const value = (event.value || '').trim();
+
+		// Add our fruit
+		if (value) {
+			this.fruits.push(value);
+		}
+		// Clear the input value
+		event.chipInput!.clear();
+
+		this.fruitCtrl.setValue(null);
+	}
+
+	remove(fruit: string): void {
+		const index = this.fruits.indexOf(fruit);
+
+		if (index >= 0) {
+			this.fruits.splice(index, 1);
+		}
+	}
+
+	selected(event: MatAutocompleteSelectedEvent): void {
+		this.fruits.push(event.option.viewValue);
+		this.fruitInput.nativeElement.value = '';
+		this.fruitCtrl.setValue(null);
+	}
+
+	private _filter(value: string): string[] {
+		const filterValue = value.toLowerCase();
+
+		return this.allFruits.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
+	}
+
+	showlabel = () => this.toggleFlag = !this.toggleFlag
 
 }
--- src/app/feature-modules/contact/contact.module.ts
+++ src/app/feature-modules/contact/contact.module.ts
@@ -5,6 +5,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact.component';
 import { MatListModule } from '@angular/material/list';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 
 const routes: Routes = [{
@@ -21,7 +25,11 @@ const Modules: any[] = [
 	MatIconModule,
 	CommonModule,
 	MatListModule,
-
+	MatChipsModule,
+	MatAutocompleteModule,
+	FormsModule,
+	ReactiveFormsModule,
+	MatInputModule,
 	[RouterModule.forChild(routes)],
 ];
 
